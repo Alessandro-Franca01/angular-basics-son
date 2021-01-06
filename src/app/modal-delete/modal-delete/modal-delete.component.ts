8003c27fbff0343354ab0e17574f4cf696df96f4
Alessandro Franca <alessandro0564@yahoo.com.br>
2021-01-06 01:33:02 -0300

    Funcionalidade update finalizado

---
+++ src/app/modal-delete/modal-delete/modal-delete.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-delete',
+  template: `
+    <p>
+      modal-delete works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ModalDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
